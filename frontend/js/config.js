15c4bfaac9f1105023eca4112592f6d53acc18ed
Lucas23-IECI <lucas.mendez2201@alumnos.ubiobio.cl>
2026-01-17 18:47:06 -0300

    chore: update API URL for production

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
     SITE_NAME: 'Mi Tienda Test',
-    API_URL: 'http://localhost:5000/api',
+    API_URL: 'https://plantillaweb-dkrs.onrender.com/api',
     DEFAULT_PRODUCT_IMAGE: '../images/products/placeholder.png',
     WHATSAPP_NUMBER: '56912345678',
     CURRENCY_LOCALE: 'es-CL',
